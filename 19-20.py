9f3a3f022a250cb8c70036da0a313f47a78092d8
matveynikon <66773302+matveynikon@users.noreply.github.com>
2022-03-27 17:42:19 +0200

    Update 19-20.py

--- 19-20.py
+++ 19-20.py
@@ -5,12 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from textblob import TextBlob
 
-DEEPGRAM_API_KEY = '32a59470034fea3fe30231e6d3a43fc65c44df91'
-reviews = ["y3/Google Pixel 3 XL Review_ The Shadow of the Notch! (128 kbps).mp3","y3/Google Pixel 3a Review_ A for Ace! (128 kbps).mp3","y3/Apple iPhone Xs Review_ A (S)mall Step Up! (128 kbps).mp3","y3/iPhone XR Review_ No Need to Panic! (128 kbps).mp3","y3/Samsung Galaxy S9 Review_ The Perfect... Samsung! (128 kbps).mp3","y3/Samsung Galaxy S10e Review_ Why Not_ (128 kbps).mp3","y3/Samsung Galaxy Note 9 Review_ The Total Package! (128 kbps).mp3","y3/Samsung Galaxy S10+ Review_ The Bar is Set! (128 kbps).mp3"]
+DEEPGRAM_API_KEY = '64bc350c1bd54bf662678a23365a941622a99ef9'
+reviews = ["y3/Google Pixel 3 XL Review_ The Shadow of the Notch! (128 kbps).mp3","y3/Apple iPhone Xs Review_ A (S)mall Step Up! (128 kbps).mp3","y3/iPhone XR Review_ No Need to Panic! (128 kbps).mp3","y3/Samsung Galaxy S9 Review_ The Perfect... Samsung! (128 kbps).mp3","y3/Samsung Galaxy S10e Review_ Why Not_ (128 kbps).mp3","y3/Samsung Galaxy Note 9 Review_ The Total Package! (128 kbps).mp3","y3/Samsung Galaxy S10+ Review_ The Bar is Set! (128 kbps).mp3"]
 async def main():
     sents = []
     ts = []
-    r2 = ["Pixel 3 XL","Pixel 3a","iPhone Xs","iPhone XR","Galaxy S9","Galaxy S10e","Galaxy Note 9","Galaxy S10+"]
+    r2 = ["Pixel 3 XL","iPhone Xs","iPhone XR","Galaxy S9","Galaxy S10e","Galaxy Note 9","Galaxy S10+"]
     for i in range(len(reviews)):
         PATH_TO_FILE = reviews[i]
         deepgram = Deepgram(DEEPGRAM_API_KEY)
@@ -26,9 +26,9 @@ async def main():
     y_pos = np.arange(len(r2))
     performance = sents
     print(sents)
-    ts.append((sents[0]+sents[1])/2)
-    ts.append((sents[2]+sents[3])/2)
-    ts.append((sents[4]+sents[5]+sents[6]+sents[7])/4)
+    ts.append(sents[0])
+    ts.append((sents[1]+sents[2])/2)
+    ts.append((sents[3]+sents[4]+sents[5]+sents[6])/4)
     print("\nAverage smartphone sentiment for each brand:")
     print("Google: "+str(ts[0]))
     print("Apple: "+str(ts[1]))
@@ -48,4 +48,3 @@ async def main():
     plt.show()
 
 asyncio.run(main())
-
